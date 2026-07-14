0d5727ebcf2ea7577230b5ee6314008a8cc1323b
kanikamishra05 <kanikarocks11@gmail.com>
2026-07-14 19:33:50 +0530

    ready for deployment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,9 +2,11 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
+
+export default api;
 
 export async function register({ username, email, password }) {
 
