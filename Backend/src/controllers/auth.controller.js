bd48be5bf0b8bcfb13d593160fa8a107f155734c
kanikamishra05 <kanikarocks11@gmail.com>
2026-07-14 21:40:36 +0530

    Fix CORS and cookie settings

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -41,7 +41,12 @@ async function registerUserController(req,res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    });
 
 
     res.status(201).json({
@@ -86,7 +91,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000 
+    });
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
@@ -111,7 +121,11 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({ token })
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
+    });
 
     res.status(200).json({
         message: "User logged out successfully"
